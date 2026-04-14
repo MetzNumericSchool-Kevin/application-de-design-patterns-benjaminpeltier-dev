6196c76f7b8152c0d8df64f787ad42df3509218b
Benjamin Peltier <ben.peltier7@gmail.com>
2026-04-14 11:45:48 +0200

    Factory pattern

---
+++ TestDesignPatterns/CharacterFactory.cs
@@ -0,0 +1,67 @@
+﻿#region FACTORY
+public abstract class CharacterFactory
+{
+    abstract public IAttacker CreateAttacker();
+
+    public void Attack()
+    {
+        IAttacker attacker = CreateAttacker();
+        attacker.Attack();
+    }
+}
+
+public class WarriorFactory : CharacterFactory
+{
+    public override IAttacker CreateAttacker()
+    {
+        return new Warrior();
+    }
+}
+
+public class ArcherFactory : CharacterFactory
+{
+    public override IAttacker CreateAttacker()
+    {
+        return new Archer();
+    }
+}
+
+public class MageFactory : CharacterFactory
+{
+    public override IAttacker CreateAttacker()
+    {
+        return new Mage();
+    }
+}
+#endregion
+
+#region CHARACTERS
+public interface IAttacker
+{
+    void Attack();
+}
+
+public class Warrior : IAttacker
+{
+    public void Attack()
+    {
+        Console.WriteLine("Warrior attacks with a sword!");
+    }
+}
+
+public class Archer : IAttacker
+{
+    public void Attack()
+    {
+        Console.WriteLine("Archer attacks with a bow!");
+    }
+}
+
+public class Mage : IAttacker
+{
+    public void Attack()
+    {
+        Console.WriteLine("Mage attacks with a spell!");
+    }
+}
+#endregion
--- TestDesignPatterns/Program.cs
+++ TestDesignPatterns/Program.cs
@@ -1,9 +1,18 @@
-﻿Console.WriteLine($"Current Difficulty: {SettingsManager.Instance.GameDifficulty}");
+﻿/*
+Console.WriteLine($"Current Difficulty: {SettingsManager.Instance.GameDifficulty}");
 Console.WriteLine($"Current Music Volume: {SettingsManager.Instance.VolumeMusic}");
 Console.WriteLine("Updating settings...");
 SettingsManager.Instance.GameDifficulty = SettingsManager.Difficulty.Hard;
 SettingsManager.Instance.VolumeMusic = 100;
 Console.WriteLine($"Current Difficulty: {SettingsManager.Instance.GameDifficulty}");
 Console.WriteLine($"Current Music Volume: {SettingsManager.Instance.VolumeMusic}");
+*/
+
+CharacterFactory warriorFactory = new WarriorFactory();
+warriorFactory.Attack();
+CharacterFactory archerFactory = new ArcherFactory();
+archerFactory.Attack();
+CharacterFactory mageFactory = new MageFactory();
+mageFactory.Attack();
 
 Console.ReadLine();
